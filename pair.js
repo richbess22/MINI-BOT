466a75974a5939818a8d952f42ebabcedb3cbeb8
DarkWinzo <darkwinzo4440@gmail.com>
2025-08-20 11:55:20 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -562,11 +562,11 @@ case 'menu': {
                     var inital = new Date().getTime();
                     let ping = await socket.sendMessage(sender, { text: '*_Pinging to Shanu Module..._* ❗' });
                     var final = new Date().getTime();
-                    await socket.sendMessage(sender, { text: '《 █▒▒▒▒▒▒▒▒▒▒▒》10%', edit: ping.key });
-                    await socket.sendMessage(sender, { text: '《 ████▒▒▒▒▒▒▒▒》30%', edit: ping.key });
-                    await socket.sendMessage(sender, { text: '《 ███████▒▒▒▒▒》50%', edit: ping.key });
-                    await socket.sendMessage(sender, { text: '《 ██████████▒▒》80%', edit: ping.key });
-                    await socket.sendMessage(sender, { text: '《 ████████████》100%', edit: ping.key });
+                    await socket.sendMessage(sender, { text: '10%', edit: ping.key });
+                    await socket.sendMessage(sender, { text: '30%', edit: ping.key });
+                    await socket.sendMessage(sender, { text: '50%', edit: ping.key });
+                    await socket.sendMessage(sender, { text: '80%', edit: ping.key });
+                    await socket.sendMessage(sender, { text: '100%', edit: ping.key });
 
                     return await socket.sendMessage(sender, {
                         text: '*Pong '+ (final - inital) + ' Ms*', edit: ping.key });
